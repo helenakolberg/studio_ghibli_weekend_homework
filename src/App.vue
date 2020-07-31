90d6c3273452845caf49493de60dd5b5e7ba6be7
Helena <inger.helena@gmail.com>
2020-07-31 11:41:08 +0100

    set up project, fetched api data and stored it in films array

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,23 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      films: []
+    }
+  },
+  mounted() {
+    fetch("https://ghibliapi.herokuapp.com/films")
+      .then(response => response.json())
+      .then(data => this.films = data)
+
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
