fa3f4ac8385e88ef9d358a66225b443593d2d763
Helena <inger.helena@gmail.com>
2020-07-31 14:27:09 +0100

    created ListItem component and linked it to FilmList

--- src/components/FilmList.vue
+++ src/components/FilmList.vue
@@ -1,15 +1,20 @@
 <template>
   <div>
       <ul>
-          <li v-for="film in films">{{ film.title }}</li>
+          <list-item v-for="(film, index) in films" :film="film" :key="index"/>
       </ul>
   </div>
 </template>
 
 <script>
+import ListItem from './ListItem'
+
 export default {
     name: 'film-list',
-    props: ['films']
+    props: ['films'],
+    components: {
+        "list-item": ListItem
+    }
 }
 </script>
 
---
+++ src/components/ListItem.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+      <li>{{ film.title }}, {{ film.release_date }}</li>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'list-item',
+    props: ['film']
+}
+</script>
+
+<style>
+
+</style>
