1d68bbbbd45eb3e41a32c4f099e70714fa10e7a8
Helena <inger.helena@gmail.com>
2020-08-02 17:08:53 +0100

    installed & imported google charts

---
+++ src/components/PieChart.vue
@@ -0,0 +1,18 @@
+<template>
+  
+</template>
+
+<script>
+import { GChart } from 'vue-google-charts'
+
+export default {
+    components: {
+        GChart
+    }
+
+}
+</script>
+
+<style>
+
+</style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueGoogleCharts from 'vue-google-charts'
+
+Vue.use(VueGoogleCharts)
+
 
 Vue.config.productionTip = false
 
